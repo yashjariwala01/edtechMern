ad7dd25a9c6ffd8e2cce77b008377aaf31fbf831
yashjariwala01 <jariwala.yash02@gmail.com>
2024-02-10 19:06:44 +0530

    version 1.02

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -29,8 +29,13 @@ function HomePage() {
     }
 
     useEffect(()=>{
-        console.log(searchRes);
-    },[searchRes])
+
+        searchKey && setSearchRes(data.filter(element=> element.name.toLowerCase().includes(searchKey.toLowerCase())))
+
+        if(searchKey === ''){
+            setSearchRes([]);
+        }
+    },[searchKey])
 
     return (
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">
@@ -42,7 +47,7 @@ function HomePage() {
                 <div>
                         <div className="flex flex-row-reverse mt-8 mb-4">
                             <button
-                                onClick={handleSearchClick}
+                                // onClick={handleSearchClick}
                                 className="px-4 py-2 bg-indigo-500 text-white rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
                             >
                                 Search
@@ -54,6 +59,31 @@ function HomePage() {
                                 className="px-4 py-2 mr-2 rounded-md border border-gray-300 focus:outline-none focus:border-indigo-500"
                                 placeholder="Search..."
                             />
+                        </div>
+
+                        <div className='flex flex-col gap-6'>
+                            <div  className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-8 mt-16">
+                                {searchRes && searchRes.map(course=>(
+                                    <div
+                                    key={course.id}
+                                    className="bg-white border p-3 shadow-lg border-black rounded cursor-pointer transition duration-300 transform hover:scale-105"
+                                    onClick={() => handleClick(course)}
+                                    >
+                                    <img className="w-full h-48 object-cover mb-4" src={`https://deltaeducation.onrender.com/assets/${course.thumbnail}`} alt={course.name} />
+                                    <div>
+                                        <p className="text-lg font-semibold">{course.name}</p>
+                                        <p className="text-gray-600">Instructor: {course.instructor}</p>
+                                        <p>Course ID: {course.id}</p>
+                                        <p>Status: {course.enrollmentStatus}</p>
+                                    </div>
+                                </div>
+                                ))}
+                            </div>
+                               <div>
+                                {
+                                    searchKey ?  <hr className='border-black border-solid '/>:null
+                                }
+                               </div>
                         </div>
 
                         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-8 mt-16">
@@ -74,6 +104,7 @@ function HomePage() {
                             </div>
                         ))}
                 </div>
+
                 </div>
             )}
         </div>
